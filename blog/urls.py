3410e3ab26149f2d7f213655921ea4a4ec098baf
Oliver Gaskell <olivergaskell700@gmail.com>
2019-07-17 14:11:55 +0100

    things

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('',views.post_list, name='post_list'),
+    path('signup/', views.SignUp.as_view(), name='signup')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    
-    
+
+
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
